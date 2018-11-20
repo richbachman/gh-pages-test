bcb888d0b9e9c21735e9449b87a95d27ba70c0d1
richbachman <rb@richbachman.com>
2018-11-20 09:25:30 -0700

    trying npm script

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var src = require('gulp');
 var ghPages = require('gulp-gh-pages');
 var useref = require('gulp-useref');
 var runSequence = require('run-sequence');
@@ -6,10 +7,10 @@ var del = require('del');
 var cached = require('gulp-cached');
 
 gulp.task('useref', function() {
-  return gulp.src('src/*.html')
+  return gulp.src('./src/*.html')
   .pipe(useref())
   .pipe(cached('useref'))
-  .pipe(gulp.dest('dist'));
+  .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('clean:dist', function(done) {
@@ -27,4 +28,7 @@ gulp.task('clean:dist', function(done) {
 
 gulp.task('build', gulp.series('clean:dist', gulp.parallel('useref')));
 
-gulp.task('deploy', () => src('./dist/**/*').pipe(ghPages()));
+gulp.task('deploy', function () {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages())
+});
